6b0903622ce331258b8ec78ae19c266fb14dec7f
stijlmassi <william.declercq@gmail.com>
2022-03-18 17:31:51 +0100

    3/18

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, {DocumentContext, Head, Html, Main, NextScript} from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        return await Document.getInitialProps(ctx)
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link href="https://fonts.googleapis.com/css2?family=Parisienne&family=Permanent+Marker&family=Roboto+Slab:wght@200&display=swap"
+                          rel="stylesheet"/>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+
+
+export default MyDocument
